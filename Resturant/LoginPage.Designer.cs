0febb77fb5bbb40c7a27388ba2fe84727d84d5e7
FAISAL MOHAMMED EZZADDIN SAIF AHMED <TP077402@mail.apu.edu.my>
2024-05-13 10:20:23 +0800

    added ingredients table , some back end work, and form editing

--- Resturant/LoginPage.Designer.cs
+++ Resturant/LoginPage.Designer.cs
@@ -93,6 +93,7 @@
             this.Name = "LoginPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginPage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
